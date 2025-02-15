b5942da068963c117862e67f68e1abc5c8391f8b
Mukezh <mukesh.offcl@gmail.com>
2025-02-15 08:40:03 +0530

    day3 morning

--- src/components/Section-two.tsx
+++ src/components/Section-two.tsx
@@ -4,7 +4,7 @@ export function Section2() {
     return(
         <div>
         <div  className="h-screen overflow-x-scroll ">
-           {/* <motion.div
+           <motion.div
         className="flex items-center whitespace-nowrap"
         animate={{ x: ["0%", "-100%"] }} // Moves content left
         transition={{ ease: "linear", duration: 8, repeat: Infinity }} // Smooth infinite loop
@@ -12,20 +12,20 @@ export function Section2() {
         
             {
                 [...Array(2)].map((_,i) => (
-                    <div key={i} className="flex items-center bg-emerald-400">
-                <div className="text-9xl p-4">
+                    <div key={i} className="flex items-center">
+                <div className="text-9xl p-4 font-bold">
                 EXPERIENCES 
                 </div>
                 <div>
                     <img className="h-18 min-w-18" src="https://awwwards-sundown-studio.vercel.app/vdonimgs/icon.png" alt="circle" />
                 </div>
-                <div className="text-9xl p-4">
+                <div className="text-9xl p-4 font-bold">
                     CONTENT 
                 </div>
                 <div>
                     <img className="h-18 min-w-18" src="https://awwwards-sundown-studio.vercel.app/vdonimgs/icon.png" alt="circle" />
                 </div>
-                <div className="text-9xl">
+                <div className="text-9xl p-4 font-bold">
                     ENVIRONMENT
                 </div>
                 <div>
@@ -34,19 +34,32 @@ export function Section2() {
             </div>
                 ))
             }
-        </motion.div> */}
+        </motion.div>
 
         <div className="flex justify-between">
-            <div className="text-6xl w-full max-w-2/3 pl-12 pr-48 text-left pt-48 font-bold">
+            <div className=" relative w-full max-w-2/3 pl-12 pr-48">
+            
+            <div >
+                <motion.div className="ml-128 mt-64 border-2 h-90 w-90 bg-[rgba(255,69,0)] border-[rgba(255,69,0)]   rounded-full blur-sm shadow-[0_0_40px_rgba(255,69,0)]"
+                animate={{scale: [1,1.2,1]}}
+                transition={{duration: 5, repeat: Infinity, ease: "easeInOut", delay: 0.4}}
+                >
+                </motion.div> 
+            </div>
+            <div className="absolute -mt-164 text-6xl text-left pt-48 pr-36 font-bold">
             We are a group of design-driven, goal-focused creators, producers, and designers who believe that the details make all the difference.
             </div>
-            <div className="text-sm w-full max-w-1/3 pl-24 pt-12">
+            </div>
+            <div className=" text-sm w-full max-w-1/3 pl-24 pt-12">
                 <img className="rounded-2xl h-60 w-96" src="https://awwwards-sundown-studio.vercel.app/vdonimgs/img01.webp" alt="" />
                 <div className="py-8 tracking-tight text-lg w-96 px-4 font-semibold">
                 We love to create, we love to solve, we love to collaborate, and we love to turn amazing ideas into reality. 
                 We’re here to partner with you through every step of the process and know that relationships are the most important things we build.
                 </div>
             </div>
+             
+        </div>
+        <div className="border border-slate-200">
         </div>
         </div>
         </div>
